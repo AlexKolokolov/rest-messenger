11fdc1171d612c76502b6c98865a928535963c86
kolokolov <kolokolov_a@ukr.net>
2016-06-23 16:38:12 +0300

    WebApplicationExaption using has been proceeded in CommentService.java. It is being thrown if required comment was not found

--- src/main/java/org/kolokolov/service/CommentService.java
+++ src/main/java/org/kolokolov/service/CommentService.java
@@ -4,29 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.kolokolov.database.DataBaseClass;
 import org.kolokolov.model.Comment;
+import org.kolokolov.model.ErrorMessage;
 import org.kolokolov.model.Message;
 
 public class CommentService {
 	
 	private Map<Long, Message> messages = DataBaseClass.getMessages();
 	
+	private ErrorMessage notFoundMessage = new ErrorMessage("Not found", 404, "http://javabrains.koushik.org");
+	private Response notFoundResponse = Response.status(Status.NOT_FOUND).entity(notFoundMessage).build();
+	
 	public List<Comment> getAllComments(long messageId) {
 		Message message = messages.get(messageId);
-		if (message == null) return null;
+		if (message == null) throw new WebApplicationException(notFoundResponse);
 		return new ArrayList<Comment>(message.getComments().values());
 	}
 	
 	public Comment getComment(Long messageId, Long commentId) {
 		Message message = messages.get(messageId);
-		if (message == null) return null;
-		return message.getComments().get(commentId);
+		if (message == null) throw new WebApplicationException(notFoundResponse);
+		Comment comment = message.getComments().get(commentId);
+		if (comment == null) throw new WebApplicationException(notFoundResponse);
+		return comment;
 	}
 	
 	public Comment addComment(Long messageId, Comment comment) {
 		Message message = messages.get(messageId);
-		if (message == null) return null;
+		if (message == null) throw new WebApplicationException(notFoundResponse);
 		comment.setId(message.getComments().size() + 1);
 		message.setComment(comment);
 		return comment;
