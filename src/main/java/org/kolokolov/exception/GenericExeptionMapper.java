2450d4d9f045cf25e2eeaa77cfd617ec31f24cdc
kolokolov <kolokolov_a@ukr.net>
2016-06-23 15:55:41 +0300

    GenericExeptionMapper.java as a default exception handler has been added

---
+++ src/main/java/org/kolokolov/exception/GenericExeptionMapper.java
@@ -0,0 +1,26 @@
+package org.kolokolov.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.kolokolov.model.ErrorMessage;
+
+@Provider
+public class GenericExeptionMapper implements ExceptionMapper<Exception>{
+	
+	public GenericExeptionMapper() {
+		System.out.println("Mapper created");
+	}
+
+	@Override
+	public Response toResponse(Exception exception) {
+		ErrorMessage errorMessage = new ErrorMessage(exception.getMessage(), 500, "http://javabrains.koushik.org");
+		return Response.
+				status(Status.INTERNAL_SERVER_ERROR).
+				entity(errorMessage).
+				build();
+	}
+
+}
